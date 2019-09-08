49feba034cf1018762e721d0da63f9ddfa3b5e81
ivanbtrujillo <ivanbtrujillo@gmail.com>
2019-09-08 16:12:47 +0100

    update(Appointment): add more fields and tests

--- src/components/Appointment/Appointment.jsx
+++ src/components/Appointment/Appointment.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
-const Appointment = ({ customer: { firstName } }) => <div>{firstName}</div>;
+const Appointment = ({
+  customer: { firstName, lastName, phoneNumber, stylist, service, notes }
+}) => (
+  <div>
+    <p>{firstName}</p>
+    <p>{lastName}</p>
+    <p>{phoneNumber}</p>
+    <p>{stylist}</p>
+    <p>{service}</p>
+    <p>{notes}</p>
+  </div>
+);
 
 export default Appointment;
--- src/components/Appointment/Appointment.test.jsx
+++ src/components/Appointment/Appointment.test.jsx
@@ -12,16 +12,42 @@ describe("Appointment", () => {
   });
 
   const render = component => ReactDOM.render(component, container);
+  customer = {
+    firstName: "Ashley",
+    lastName: "Hamilton",
+    phoneNumber: 666777888,
+    stylist: "Ana White",
+    service: "hair cut",
+    notes: "will arrive 10 min later"
+  };
 
-  it("renders the customer first name", () => {
-    customer = { firstName: "Ashley" };
+  it("renders the customer firstName", () => {
     render(<Appointment customer={customer} />);
-    expect(container.textContent).toEqual("Ashley");
+    expect(container.textContent).toMatch("Ashley");
   });
 
-  it("renders another customer first name", () => {
-    customer = { firstName: "Jordan" };
+  it("renders customer lastName", () => {
     render(<Appointment customer={customer} />);
-    expect(container.textContent).toEqual("Jordan");
+    expect(container.textContent).toMatch("Hamilton");
+  });
+
+  it("renders phone number", () => {
+    render(<Appointment customer={customer} />);
+    expect(container.textContent).toMatch("666777888");
+  });
+
+  it("renders stylist", () => {
+    render(<Appointment customer={customer} />);
+    expect(container.textContent).toMatch("Ana White");
+  });
+
+  it("renders service", () => {
+    render(<Appointment customer={customer} />);
+    expect(container.textContent).toMatch("hair cut");
+  });
+
+  it("renders notes", () => {
+    render(<Appointment customer={customer} />);
+    expect(container.textContent).toMatch("will arrive 10 min later");
   });
 });
