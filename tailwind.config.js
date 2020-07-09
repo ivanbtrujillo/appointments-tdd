6bdfafd5d7194ebb673f0a4aca54b23abd4c1ec0
ivanbtrujillo <ivanbtrujillo@gmail.com>
2020-07-09 17:46:06 +0100

    update(tailwind config): configure purgue to remove unnecesary css in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"],
   theme: {
-    extend: {}
+    extend: {},
   },
   variants: {},
-  plugins: []
-}
+  plugins: [],
+};
